2f7742edab7f89d77263d12f62cbb00fce386466
austenjs <e0407786u.nus.edu>
2021-08-23 02:51:47 +0800

    fix error in logic of the code

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -4,17 +4,17 @@ import cv2
 import numpy as np
 import pandas as pd
 
-def load_data(dataset, img_height, img_width):
+def load_data(path_to_csv, path_to_images, img_height, img_width):
   images = []
   classes = []
-  rows = pd.read_csv(dataset)
+  rows = pd.read_csv(path_to_csv)
 
   for i, row in rows.iterrows():
     img_class = row["ClassId"]
     img_path = row["Path"]
 
-    image = os.path.join(data, img_path)
-    image = cv2.imread(image)
+    path_to_image = os.path.join(path_to_images, img_path)
+    image = cv2.imread(path_to_image)
     image = cv2.resize(image, (img_height, img_width), 3)
     
     if i % 500 == 0:
